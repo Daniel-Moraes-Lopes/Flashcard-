7323b7a2122c0695fdc5ccca2bf642c7fd451fa4
Daniel-Moraes-Lopes <daniel.moraes.lopes@escola.pr.gov.br>
2025-07-01 21:48:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o Imperador mais importante da China',
+    'Qin Shi Huang (259-210 a.C.). Responsável por unificar a china pela primeira vez'
+)
+
+criaCartao(
+    'Química',
+    'Qual elemento é mais radioativo dentre todos',
+    ' Polônio-210. É um isótopo radioativo do elemento químico polônio, conhecido por sua alta toxicidade.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Física',
+    'Quem foi o físico mais importante',
+    'Albert Einstein, devido às suas revolucionárias teorias da relatividade e suas contribuições fundamentais para a mecânica quântica e a física estatística.'
+)
